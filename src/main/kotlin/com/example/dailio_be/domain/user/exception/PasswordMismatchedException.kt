8a25f1b869ea0018bd90229f8be122a51c52a6bd
sorim <enhld00@gmail.com>
2025-01-19 19:32:54 +0900

    delete : 예외 클래스 삭제

--- src/main/kotlin/com/example/dailio_be/domain/user/exception/PasswordMismatchedException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dailio_be.domain.user.exception
-
-import com.example.dailio_be.global.error.CustomException
-import com.example.dailio_be.global.error.ErrorCode
-
-object PasswordMismatchedException : CustomException(ErrorCode.PASSWORD_MISMATCHED)
