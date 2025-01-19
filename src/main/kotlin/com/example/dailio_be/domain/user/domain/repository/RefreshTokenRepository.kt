f522ad387e134e4aca0693177f8973d43df12200
sorim <enhld00@gmail.com>
2025-01-19 19:26:43 +0900

    refactor : CrudRepository로 변경

--- src/main/kotlin/com/example/dailio_be/domain/user/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/dailio_be/domain/user/domain/repository/RefreshTokenRepository.kt
@@ -1,8 +1,8 @@
 package com.example.dailio_be.domain.user.domain.repository
 
 import com.example.dailio_be.domain.user.domain.RefreshToken
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RefreshTokenRepository : JpaRepository<RefreshToken, String>
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
